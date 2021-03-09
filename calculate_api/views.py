b9e027f14605e87f7a5a3ba0bd436d6e7114ce7b
irfanmaulananasution <irfanmaulananasution@gmail.com>
2021-03-09 08:14:52 +0700

    put readme. and did some cleaning

--- calculate_api/views.py
+++ calculate_api/views.py
@@ -1,11 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
 from braces.views import CsrfExemptMixin
-import requests
-import sys
-import json
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 #https://www.django-rest-framework.org/api-guide/status-codes/
 #https://stackoverflow.com/a/34316546/4921103
@@ -43,12 +40,3 @@ class Calculator(CsrfExemptMixin, APIView):
             return Response({}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
         except:
             return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-# Create your views here.    
-
-def pretty_print_POST(req):
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
